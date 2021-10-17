55026a935c0543a79cdcdb36c17163fc2d98a59f
unknown <joselitomartinho@gmail.com>
2021-10-17 17:48:11 -0300

    Typescript parte 2 - Aula 04 - Lapidando nosso código.

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -9,7 +9,7 @@ export class NegociacaoController {
     private inputQuantidade: HTMLInputElement;
     private inputValor: HTMLInputElement;
     private negociacoes = new Negociacoes();
-    private negociacoesView = new NegociacoesView('#negociacoesView');
+    private negociacoesView = new NegociacoesView('#negociacoesView', true);
     private mensagemView = new MensagemView('#mensagemView');
 
     constructor() {
@@ -20,7 +20,12 @@ export class NegociacaoController {
     }
 
     adiciona(): void {
-        const negociacao = this.criaNegociacao();
+        const negociacao = Negociacao.criaDe(
+            this.inputData.value,
+            this.inputQuantidade.value,
+            this.inputValor.value
+        );
+
         if (!this.ehDiaUtil(negociacao.data)) {
             this.mensagemView.update('Apenas negociações em dias úteis são aceitas.');
             return;
@@ -35,14 +40,14 @@ export class NegociacaoController {
         return data.getDay() > DiasDaSemana.DOMINGO && data.getDay() < DiasDaSemana.SABADO;
     }
 
-    private criaNegociacao(): Negociacao {
-        const expReg = /-/g;
-        const date = new Date(this.inputData.value.replace(expReg, ','));
-        const quantidade = parseInt(this.inputQuantidade.value);
-        const valor = parseFloat(this.inputValor.value);
+    // private criaNegociacao(): Negociacao {
+        // const expReg = /-/g;
+        // const date = new Date(this.inputData.value.replace(expReg, ','));
+        // const quantidade = parseInt(this.inputQuantidade.value);
+        // const valor = parseFloat(this.inputValor.value);
         
-        return new Negociacao(date, quantidade, valor);
-    }
+        // return new Negociacao(date, quantidade, valor);
+    // }
 
     private limparFormulario(): void {
         this.inputData.value = '';
